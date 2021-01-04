9d456b322f3f322d4615a031deeecd232354cc66
Daniel <danielsaraldi@gmail.com>
2021-01-04 20:34:02 -0300

    Starting DS Delivery Project and Creating backend work area.

---
+++ backend/src/main/java/com/dsdeliveryproject/dsdelivery/DsdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.dsdeliveryproject.dsdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dsdeliveryproject/dsdelivery/DsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsdeliveryproject.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
