d24875a0367f520f047c6266b64425ae3f32c5ff
Daniel <danielsaraldi@gmail.com>
2021-01-06 20:15:59 -0300

    Creating POST of orders.

--- backend/src/main/java/com/dsdeliveryproject/dsdelivery/controllers/OrderController.java
+++ backend/src/main/java/com/dsdeliveryproject/dsdelivery/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.dsdeliveryproject.dsdelivery.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dsdeliveryproject.dsdelivery.dto.OrderDTO;
 import com.dsdeliveryproject.dsdelivery.services.OrderService;
@@ -24,4 +28,11 @@ public class OrderController {
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri(); /* Creating URI */
+		return ResponseEntity.created(uri).body(dto);
+	} 
 }
--- backend/src/main/java/com/dsdeliveryproject/dsdelivery/services/OrderService.java
+++ backend/src/main/java/com/dsdeliveryproject/dsdelivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.dsdeliveryproject.dsdelivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dsdeliveryproject.dsdelivery.dto.OrderDTO;
+import com.dsdeliveryproject.dsdelivery.dto.ProductDTO;
 import com.dsdeliveryproject.dsdelivery.entities.Order;
+import com.dsdeliveryproject.dsdelivery.entities.OrderStatus;
+import com.dsdeliveryproject.dsdelivery.entities.Product;
 import com.dsdeliveryproject.dsdelivery.repositories.OrderRepository;
+import com.dsdeliveryproject.dsdelivery.repositories.ProductRepository;
 
 // Informs that this class is a service component
 @Service
@@ -17,6 +22,9 @@ public class OrderService {
 
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired 
+	private ProductRepository productRepository;
 
 	// Return a DTO (Data Transfer Objects), that is, it will only deliver the data
 	// you send
@@ -26,4 +34,17 @@ public class OrderService {
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
 
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		
+		// Product association to order
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
 }
