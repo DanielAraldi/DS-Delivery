335d71d1ca26737e470aa893833458c09321b21b
Daniel <danielsaraldi@gmail.com>
2021-01-08 21:18:28 -0300

    Created Navbar, your styles and some formatting.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+import { ReactComponent as Logo } from "../../assets/logo.svg"; /* Name the image */
+
+function Navbar() {
+  return (
+    <nav className="main-navbar">
+      <Logo />
+      <a href="/" className="logo-text">
+        DS Delivery
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
