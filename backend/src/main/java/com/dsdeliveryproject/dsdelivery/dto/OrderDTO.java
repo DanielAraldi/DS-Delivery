0ef1c6e49af8d642380f04f499387025bb78179c
Daniel <danielsaraldi@gmail.com>
2021-01-11 20:00:51 -0300

    Fix and insert on total price.

--- backend/src/main/java/com/dsdeliveryproject/dsdelivery/dto/OrderDTO.java
+++ backend/src/main/java/com/dsdeliveryproject/dsdelivery/dto/OrderDTO.java
@@ -19,6 +19,7 @@ public class OrderDTO implements Serializable {
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
+	private Double total;
 
 	private List<ProductDTO> products = new ArrayList<>();
 
@@ -26,13 +27,15 @@ public class OrderDTO implements Serializable {
 
 	}
 
-	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status,
+			Double total) {
 		this.id = id;
 		this.address = address;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.moment = moment;
 		this.status = status;
+		this.total = total;
 	}
 
 	public OrderDTO(Order entity) {
@@ -42,6 +45,7 @@ public class OrderDTO implements Serializable {
 		longitude = entity.getLongitude();
 		moment = entity.getMoment();
 		status = entity.getStatus();
+		total = entity.getTotal();
 		products = entity.getProducts().stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
 
@@ -91,6 +95,14 @@ public class OrderDTO implements Serializable {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
 	}
 
 	public List<ProductDTO> getProducts() {
--- backend/src/main/java/com/dsdeliveryproject/dsdelivery/entities/Order.java
+++ backend/src/main/java/com/dsdeliveryproject/dsdelivery/entities/Order.java
@@ -99,6 +99,15 @@ public class Order implements Serializable {
 		this.status = status;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (Product p : products) {
+			sum += p.getPrice();
+		}
+
+		return sum;
+	}
+
 	public Set<Product> getProducts() {
 		return products;
 	}
